e1bda681eac90dd6a476417505ad5d330ea69942
darth-veitcher <1722315+darth-veitcher@users.noreply.github.com>
2025-10-17 09:38:12 +0100

    fix playwright startup install

--- .devcontainer/playwright.sh
+++ .devcontainer/playwright.sh
@@ -6,7 +6,7 @@ set -e
 # https://blog.jongallant.com/2021/08/playwright-codegen-devcontainer/
 export DEBIAN_FRONTEND=noninteractive
 sudo apt-get update && \
-sudo npx playwright install-deps && \
+echo "\n" | sudo npx -q playwright install-deps && \
 npx -q playwright install --force --with-deps webkit && \
 # sudo npm i -D @playwright/test && \
 # sudo npx -q playwright install && \
